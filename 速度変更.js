73466a5771f2747ffc4d2799a27d04bb362f14cb
takanaweb5 <50874513+takanaweb5@users.noreply.github.com>
2021-05-01 09:02:41 +0900

    Delete 速度変更.js

--- 速度変更.js
+++
@@ -1,9 +0,0 @@
-javascript: {
-    let copyListener = event => {
-        document.removeEventListener("copy", copyListener, true);
-        event.preventDefault();
-        let clipboardData = event.clipboardData;
-        clipboardData.clearData();
-        clipboardData.setData("text/plain", "abc");
-    };document.addEventListener("copy", copyListener, true);document.execCommand("copy");
-}
